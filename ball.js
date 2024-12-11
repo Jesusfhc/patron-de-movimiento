bee622198aaec6434c79b1a0b1cda76563c475f7
Jesusfhc <jesus.hernandez.sym@gmail.com>
2024-12-11 16:46:13 -0400

    paso 2 listo creadas mas balls

--- ball.js
+++ ball.js
@@ -1,9 +1,12 @@
 class Ball {
-    
+
     constructor(x,y) {
         this.x = x;
         this.y = y;
         this.radio = 10;
+        this.dirX = (Math.random() * 2) -1;
+        this.dirY = (Math.random() * 2) -1;
+        this.velocidad = 5;
     }
 
     dibujar() {
@@ -11,5 +14,12 @@ class Ball {
         ctx.arc(this.x, this.y, this.radio, 0, Math.PI * 2);
         ctx.fill();
         ctx.closePath();
+    }
+
+    mover() {
+        this.x += this.dirX * this.velocidad;
+        this.y += this.dirY * this.velocidad;
+
+
     }
 }
--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const ctx = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-let ball = new Ball (canvas.width / 2, canvas.height / 2);
+let balls = [];
 
-ball.dibujar();
+for (let i =0 ; i < 5; i++) {
+    balls.push(new Ball(canvas.width / 2, canvas.height / 2))
+}
+
+
+function animar () {
+
+    balls.forEach(ball => {
+        ball.dibujar();
+        ball.mover();
+    })
+
+    requestAnimationFrame(animar)
+}
+
+animar();
