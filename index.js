b23c042455c083e0312ab75a0259b82e8f45fde5
Jesusfhc <jesus.hernandez.sym@gmail.com>
2024-12-12 09:45:38 -0400

    practica finalizada

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ canvas.height = innerHeight;
 
 let balls = [];
 
-for (let i =0 ; i < 50; i++) {
+for (let i =0 ; i < 10; i++) {
     balls.push(new Ball(canvas.width / 2, canvas.height / 2))
 }
 
@@ -16,6 +16,27 @@ function animar () {
     ctx.clearRect(0,0, canvas.width, canvas.height);
 
     balls.forEach(ball => {
+
+
+        balls.forEach(ballT => {
+
+            let dx = ball.x - ballT.x;
+            let dy = ball.y - ballT.y;
+
+            let dist = Math.sqrt(dx ** 2 + dy ** 2);
+
+            if (dist < 300) {
+                ctx.beginPath();
+                ctx.moveTo(ball.x, ball.y);
+                ctx.lineTo(ballT.x, ballT.y);
+                ctx.stroke();
+                ctx.closePath();
+            }
+
+
+            
+        })
+
         ball.dibujar();
         ball.mover();
     })
